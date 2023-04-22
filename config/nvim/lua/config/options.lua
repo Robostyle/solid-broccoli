18c092e61abeaf46a001d77346d2423b83a9e13e
Robert Suijker <robert.suijker@gmail.com>
2023-04-22 21:53:16 +0200

    added initial nvim (LazyVim) based config files

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,20 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+--
+
+local opt = vim.opt
+
+opt.winbar = "%=%m %f"
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+
+opt.termguicolors = true
+
+opt.numberwidth = 5
+
+opt.cursorline = true
---
+++ config/nvim/lua/plugins/color-converter.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "NTBBloodbath/color-converter.nvim",
+
+    keys = {
+      { "<leader>ccr", "<Plug>ColorConvertRGB<cr>", desc = "Color convert to RGB" },
+      { "<leader>cch", "<Plug>ColorConvertHEX<cr>", desc = "Color conveert to HEX" },
+    },
+  },
+}
