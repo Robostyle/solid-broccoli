78ab42b585eaed0845c184c1be339e4dba1a6a70
Robert Suijker <robert.suijker@gmail.com>
2023-05-23 18:20:42 +0200

    nvim: added nav-buddy

---
+++ config/nvim/lua/plugins/nav-budy.lua
@@ -0,0 +1,17 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "SmiteshP/nvim-navbuddy",
+      dependencies = {
+        "SmiteshP/nvim-navic",
+        "MunifTanjim/nui.nvim",
+      },
+      opts = { lsp = { auto_attach = true } },
+    },
+  },
+
+  keys = {
+    { "<leader>n", ":Navbuddy<cr>", desc = "Nav Buddy" },
+  },
+}
