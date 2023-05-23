1d543efd5c071b03b2c26acec6c1f6b94b072d71
Robert Suijker <robert.suijker@gmail.com>
2023-05-23 18:23:35 +0200

    nvim: updated some options

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -4,17 +4,15 @@
 
 local opt = vim.opt
 
-opt.winbar = "%=%m %f"
-
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
 opt.autoindent = true
-opt.smartindent = true
-
-opt.termguicolors = true
-
-opt.numberwidth = 5
-
 opt.cursorline = true
+opt.expandtab = true
+opt.formatoptions = "jcroqlnt"
+opt.numberwidth = 5
+opt.scrolloff = 8
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.termguicolors = true
+opt.winbar = "%=%m %f"
