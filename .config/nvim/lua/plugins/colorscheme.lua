2ea5b8048e4a1ef229dc40bac412fce2e783658f
Robert Suijker <Robert Suijker r.suijker@tkhsecurity.com>
2023-06-22 22:07:27 +0200

    osx: LazyVim plugin case matters

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,23 +11,9 @@ return {
   },
 
   {
-    "lazyVim/lazyVim",
+    "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
   },
 }
-
--- return {
---   {
---     "folke/tokyonight.nvim",
---     opts = {
---       transparent = true,
---       styles = {
---         sidebars = "transparent",
---         floats = "dark",
---       },
---       dim_inactive = true,
---     },
---   },
--- }
